71f947d943c406b50e0ca614df21b8480bec8dc3
DarkAstaroth <mninahuanca4@gmail.com>
2021-10-27 17:22:40 -0400

    Comienzo de proyecto

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+// import React from "react"
+
+// export const onRenderBody = ({ setPostBodyComponents }) => {
+//   setPostBodyComponents([
+//     <script
+//       key="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+//       src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+//       integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+//       crossOrigin="anonymous"
+//       defer
+//     />,
+//     <script
+//       key="/plugins.js"
+//       src="/plugins.js"
+//       //   integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+//       //   crossOrigin="anonymous"
+//       //   defer
+//     />,
+//   ])
+// }
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,90 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg fancy navbar-light navbar-bg-light caret-none">
+        <div className="container">
+          <div className="navbar-collapse-wrapper bg-white d-flex flex-row flex-nowrap w-100 justify-content-between align-items-center">
+            <div className="navbar-brand w-100">
+              <a href="./index.html">
+                {/* <img
+                  src="./assets/img/logo.png"
+                  srcSet="./assets/img/logo@2x.png 2x"
+                  alt=""
+                /> */}
+              </a>
+            </div>
+            <div className="navbar-collapse offcanvas-nav">
+              <div className="offcanvas-header d-lg-none d-xl-none">
+                <a href="./index.html">
+                  {/* <img
+                    src="./assets/img/logo-light.png"
+                    srcSet="./assets/img/logo-light@2x.png 2x"
+                    alt=""
+                  /> */}
+                </a>
+                <button
+                  type="button"
+                  className="btn-close btn-close-white offcanvas-close offcanvas-nav-close"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="!#">
+                    Home
+                  </a>
+                </li>
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+                <li className="nav-item">
+                  <a className="nav-link" href="!#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="!#">
+                    Skills
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="!#">
+                    Portfolio
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-Header.defaultProps = {
-  siteTitle: ``,
+            <div className="navbar-other w-100 d-flex ms-auto">
+              <ul
+                className="navbar-nav flex-row align-items-center ms-auto"
+                data-sm-skip="true"
+              >
+                <li className="nav-item d-none d-md-block">
+                  <a
+                    href="./contact.html"
+                    className="btn btn-sm btn-primary rounded-pill"
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item d-lg-none">
+                  <div className="navbar-hamburger">
+                    <button
+                      className="hamburger animate plain"
+                      data-toggle="offcanvas-nav"
+                    >
+                      <span></span>
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Header
